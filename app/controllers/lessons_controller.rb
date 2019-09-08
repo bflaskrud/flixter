f3d5f12f111bf9f704cc083932c7c944545ee53c
Ben Flaskrud <bflaskrud2@yahoo.com>
2019-09-08 20:45:21 +0000

    finished flixter links

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,22 +1,24 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-  #before_action :require_authorized_for_current_lesson, only: [:show]
+  before_action :require_authorized_for_current_lesson, only: [:show]
 
   def show
   end
 
   private
 
-  #def require_authorized_for_current_lesson
-  #  if current_user.enrolled_in?(current_lesson.section.course) != current_user
-  #    redirect_to course_path, alert: 'Please enroll to view.'
-  #  end
-  # end
+  def require_authorized_for_current_lesson
+    unless current_user.enrolled_in?(current_course)
+      redirect_to course_path(current_course), alert: 'Please enroll to view.'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  def current_course
+    current_lesson.section.course
+  end
 end
-
-
